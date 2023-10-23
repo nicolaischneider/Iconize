0b31f2ba8081c918d0ecac564ab4fb8a8f1e7618
Nicolai Schneider <nicolai-schneider@t-online.de>
2023-10-23 16:08:38 +0200

    added setup fiule

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iconize",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow",
+    ],
+    entry_points={
+        'console_scripts': [
+            'iconize = Iconize:main',
+        ],
+    },
+)
